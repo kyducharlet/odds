27435b263c76154496248920bae4325635315f79
Kevin.DUCHARLET <kevin.ducharlet@berger-levrault.com>
2023-12-19 15:22:34 +0100

    Remove faust for now

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email='kevin.ducharlet@carl.eu',
     description='Framework for unsupervised outlier detection on datastreams.',
     install_requires=['numpy==1.26.1', 'matplotlib==3.8.1', 'scipy==1.11.3', 'scikit-learn==1.3.2', 'pandas==2.1.2', 'smartsifter==0.1.1.dev1', 'tqdm==4.66.1', 'openpyxl==3.2.0b1', 'pympler==1.0.1',
-                      'pyfaust==3.39.19'],
+                      ],
     python_requires='~=3.11',
 )
